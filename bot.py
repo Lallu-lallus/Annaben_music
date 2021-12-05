409494e980210b07f5445e36e4fa21a20a21d8c2
Ashik Muhammed <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-12-05 12:25:09 +0530

    Stuff by oxidisedman #2

--- bot.py
+++ bot.py
@@ -381,4 +381,45 @@ async def lrsearch(_, message: Message):
 {S.lyrics}"""
     await m.edit(xxx)
 
+@Bot.on_message(filters.command(['find']))
+
+async def shazamm(client, message):
+    kek = await edit_or_reply(message, "`i'm seraching your song..`")
+    if not message.reply_to_message:
+        await kek.edit("Reply To The Audio.")
+        return
+    if os.path.exists("find.mp3"):
+        os.remove("find.mp3")
+    kkk = await fetch_audio(client, message)
+    downloaded_file_name = kkk
+    f = {"file": (downloaded_file_name, open(downloaded_file_name, "rb"))}
+    await kek.edit("`Searching Your song in YouTube.`")
+    r = requests.post("https://starkapi.herokuapp.com/shazam/", files=f)
+    try:
+        xo = r.json()
+    except JSONDecodeError:
+        await kek.edit(
+            "`Seems Like Our `SHAZAM` Has Some Issues, Please Try Again Later!`"
+        )
+        return
+    if xo.get("success") is False:
+        await kek.edit("`Song Not Found Please Try Again.`")
+        os.remove(downloaded_file_name)
+        return
+    xoo = xo.get("response")
+    zz = xoo[1]
+    zzz = zz.get("track")
+    zzz.get("sections")[3]
+    nt = zzz.get("images")
+    image = nt.get("coverarthq")
+    by = zzz.get("subtitle")
+    title = zzz.get("title")
+    messageo = f"""<b>🎶Song Founded.</b>
+<b>📀Song Name : </b>{title}
+<b>🆙Uploaded By : </b>{by}
+"""
+    await client.send_photo(message.chat.id, image, messageo, parse_mode="HTML")
+    os.remove(downloaded_file_name)
+    await kek.delete()
+
 Bot.run()
