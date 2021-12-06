8c65a8e684bccb19c68d5dba505b8e34ba14a7ad
Ashik Muhammed <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-12-06 21:44:12 +0530

    😊

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config(object):
     DATABASE = os.environ.get("DB_URI")
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1961162381").split())
-   
+    SUPPORT = os.environ.get("SUPPORT")
