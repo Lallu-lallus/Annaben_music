bc55ba14ff77d0742920f83df717ccabb1684460
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-12-11 19:14:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -222,7 +222,7 @@ def a(client, message):
         message.reply_audio(audio_file, caption=rep, parse_mode='HTML',quote=False, title=title, duration=dur, performer=performer, thumb=thumb_name)
         m.delete()
     except Exception as e:
-        m.edit('**Something Went Wrong Report This at @NAZRIYASUPPORT!!**')
+        m.edit('**Something Went Wrong Report This at @LALLU_TG!!**')
         print(e)
     try:
         os.remove(audio_file)
