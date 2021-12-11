def4595339c9b6300893ee908c59455d06a41c65
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-12-11 22:29:13 +0530

    Update info.py

--- plugins/info.py
+++ plugins/info.py
@@ -1,2 +1,14 @@
+import re
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
 # Command
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
